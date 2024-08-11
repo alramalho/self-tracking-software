cfde31c31f0b9062453e2af0b2f716e2d4e64502
alramalho <alexandre.ramalho.1998@gmail.com>
2024-08-11 01:45:09 +0100

    pwa

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const withPWA = require('next-pwa')({
+    dest: 'public'
+  })
+  
+export default withPWA({});
