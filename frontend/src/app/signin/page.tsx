a0c9d0812133cacfb4009068c085da242c50ed61
alramalho <alexandre.ramalho.1998@gmail.com>
2024-08-27 12:02:13 +0200

    * fix(signin/page.tsx): update sign up URL to "/signup" * fix(signup/page.tsx): update sign in URL to "/signin"

--- frontend/src/app/signin/page.tsx
+++ frontend/src/app/signin/page.tsx
@@ -7,7 +7,7 @@ import React from "react";
 const SignUpPage: React.FC = () => {
   return (
     <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      <SignIn signUpUrl="/sign-up" />
+      <SignIn signUpUrl="/signup" />
     </div>
   );
 };
