4e7b252057af9e37c4eb81e5afc1ef81d60ccbfd
alramalho <alexandre.ramalho.1998@gmail.com>
2024-09-03 13:27:44 +0200

    rm pwa page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -10,18 +10,12 @@ export default function Home() {
       <h1 className="text-4xl mb-4">Welcome to tracking.so</h1>
       {isSignedIn ? (
         <>
-        <Link
-          href="/log"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Go to Log Page
-        </Link>
-        <Link
-          href="/pwa"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Go to PWA Page
-        </Link>
+          <Link
+            href="/log"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Go to Log Page
+          </Link>
         </>
       ) : (
         <Link
