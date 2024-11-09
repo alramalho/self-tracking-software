588af37cc4512302bcfbb563214f5a1d44272b59
alramalho <alexandre.ramalho.1998@gmail.com>
2024-11-09 22:52:04 +0100

    update constants.py

--- backend/constants.py
+++ backend/constants.py
@@ -29,4 +29,4 @@ JINA_API_KEY=os.getenv("JINA_API_KEY")
 URL = "http://localhost:3000" if ENVIRONMENT == "dev" else "https://app.tracking.so"
 
 POSTHOG_API_KEY=os.getenv("POSTHOG_API_KEY")
-POSTHOG_HOST=os.getenv("POSTHOG_HOST")
+POSTHOG_HOST="https://eu.i.posthog.com"
