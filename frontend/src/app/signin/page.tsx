c9e33f6ba24161704358d9fbe388b2fd1d26ba50
alramalho <alexandre.ramalho.1998@gmail.com>
2024-11-09 23:17:40 +0100

    update page.tsx

--- frontend/src/app/signin/page.tsx
+++ frontend/src/app/signin/page.tsx
@@ -11,7 +11,7 @@ const SignUpPage: React.FC = () => {
 
   return (
     <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      <SignIn signUpUrl={redirectUrl ? "/signup?redirect_url=" + redirectUrl : "/signup"} forceRedirectUrl={redirectUrl} callbackUrl={redirectUrl} />
+      <SignIn signUpUrl={redirectUrl ? "/signup?redirect_url=" + redirectUrl : "/signup"} forceRedirectUrl={redirectUrl} />
     </div>
   );
 };
