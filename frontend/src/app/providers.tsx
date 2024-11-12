d06521fbe04463b857ddf2644ee9a1120c7870ab
alramalho <alexandre.ramalho.1998@gmail.com>
2024-11-12 10:38:02 +0100

    posthog rewrites

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -10,9 +10,10 @@ export function PHProvider({ children }: { children: React.ReactNode }) {
       process.env.NEXT_PUBLIC_POSTHOG_HOST
     ) {
       posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+        api_host: "/ingest",
+        ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
         person_profiles: "identified_only",
-        capture_pageview: false, 
+        capture_pageview: false,
       });
     } 
     }, []);
