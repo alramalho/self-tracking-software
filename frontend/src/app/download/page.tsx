e79a5221f689a574cd86210d58ccdeced18b21c9
alramalho <alexandre.ramalho.1998@gmail.com>
2024-11-15 10:53:58 +0100

    download page

---
+++ frontend/src/app/download/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AppNotInstalledPage from "@/components/AppNotInstalledPage";
+
+const DownloadPage: React.FC = () => {
+  return <AppNotInstalledPage />;
+};
+
+export default DownloadPage;
