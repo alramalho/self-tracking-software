d0af0e836bd3d610e83eb4d58bbcc91d34616179
alramalho <alexandre.ramalho.1998@gmail.com>
2024-11-15 14:31:43 +0100

    popover fix

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -4,7 +4,6 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { Loader2 } from "lucide-react"
 
 import { cn } from "@/lib/utils"
-import { Fascinate } from "next/font/google"
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
