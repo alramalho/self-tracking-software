42fcbb499ecf780b16007e09783d2d65f4c8bf34
alramalho <alexandre.ramalho.1998@gmail.com>
2024-11-21 23:21:28 +0100

    display other activites on profile

--- frontend/src/components/Divider.tsx
+++ frontend/src/components/Divider.tsx
@@ -2,15 +2,16 @@ import React from "react";
 
 interface DividerProps {
   text?: string;
+  className?: string;
 }
 
-const Divider: React.FC<DividerProps> = ({ text }) => {
+const Divider: React.FC<DividerProps> = ({ text, className }) => {
   return (
-    <div className="flex items-center my-4">
+    <div className={`flex items-center my-4 ${className}`}>
       <div className="flex-grow h-[1px] bg-gray-200" />
       {text && (
         <span className="px-4 text-sm text-gray-500">
-          {text}
+          {text.toUpperCase()}
         </span>
       )}
       <div className="flex-grow h-[1px] bg-gray-200" />
