f705593b70efc17c50fc249d8021470ee5c3f7e3
alramalho <alexandre.ramalho.1998@gmail.com>
2024-11-22 00:28:03 +0100

    better loading

--- frontend/src/app/layoutClient.tsx
+++ frontend/src/app/layoutClient.tsx
@@ -5,11 +5,12 @@ import BottomNav from "../components/BottomNav";
 import { Toaster } from "react-hot-toast";
 import { UserPlanProviderWrapper } from "@/components/UserPlanProviderWrapper";
 import { NotificationsProvider } from "@/hooks/useNotifications";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import GeneralInitializer from "@/components/GeneralInitializer";
+import Link from "next/link";
 
 const queryClient = new QueryClient();
 
@@ -20,11 +21,29 @@ export default function ClientLayout({
 }) {
   const { isSignedIn, isLoaded } = useSession();
 
+  const [showServerMessage, setShowServerMessage] = useState(false);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowServerMessage(true);
+    }, 4000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   if (!isLoaded) {
+
     return (
-      <div className="flex flex-col items-center justify-center h-screen">
-        <Loader2 className="w-8 h-8 animate-spin" />
-        <p className="mt-2">Loading app</p>
+      <div className="h-screen flex items-center justify-center">
+        <Loader2 className="w-10 h-10 animate-spin mr-3" />
+        <div className="flex flex-col items-start">
+          <p className="text-left">Loading your data...</p>
+          {showServerMessage && (
+            <span className="text-gray-500 text-sm text-left">
+              we run on cheap servers...<br/>first request of the day always takes longer<br/>
+              <Link target="_blank" href="https://ko-fi.com/alexramalho" className="underline">donate?</Link>
+            </span>
+          )}
+        </div>
       </div>
     );
   }
