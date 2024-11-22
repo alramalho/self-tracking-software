d79b4fc790106cae69e9875f80958c1d6021ce2e
alramalho <alexandre.ramalho.1998@gmail.com>
2024-11-22 01:23:46 +0100

    provisioned concurrency 2

--- aws-infrastructure/src/api-stack.ts
+++ aws-infrastructure/src/api-stack.ts
@@ -57,7 +57,7 @@ export class ApiStack extends cdk.Stack {
     const aliasOptions = {
       aliasName: "live",
       version: version,
-      provisionedConcurrentExecutions: 1,
+      provisionedConcurrentExecutions: 2,
     };
 
     const alias = new lambda.Alias(this, "BackendLambdaAlias", aliasOptions);
