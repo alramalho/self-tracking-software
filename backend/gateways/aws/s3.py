51fb16e247d8446434690872952e82ae691a1964
alramalho <alexandre.ramalho.1998@gmail.com>
2024-11-22 11:00:07 +0100

    add missing aws configuration

--- backend/gateways/aws/s3.py
+++ backend/gateways/aws/s3.py
@@ -9,14 +9,10 @@ class S3Gateway:
     def __init__(self):
         self.s3_client = boto3.client(
             "s3",
-            aws_access_key_id=AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
             region_name="eu-central-1",
         )
         self.bucket = boto3.resource(
             "s3",
-            aws_access_key_id=AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
             region_name="eu-central-1",
         ).Bucket(S3_BUCKET_NAME)
 
