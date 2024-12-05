31e90dd3c1ab1ff1a658b59c2c4ee97aa834304c
alramalho <alexandre.ramalho.1998@gmail.com>
2024-12-05 18:41:37 +0100

    reshow emotion viewer

--- frontend/src/app/profile/[username]/ProfilePage.tsx
+++ frontend/src/app/profile/[username]/ProfilePage.tsx
@@ -355,10 +355,10 @@ const ProfilePage: React.FC = () => {
             </div>
           </AppleLikePopover>
         )}
-{/* 
+
         <div className="w-full max-w-3xl mb-8">
           <EmotionViewer messages={profileData.messages} />
-        </div> */}
+        </div>
 
         <Tabs defaultValue="plans" className="w-full">
           <TabsList className="grid w-full grid-cols-2 h-13">
