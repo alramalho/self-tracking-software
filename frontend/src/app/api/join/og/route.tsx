2b260981ba6d2b3df9761472cf4d5a6b40a1d07b
alramalho <alexandre.ramalho.1998@gmail.com>
2025-01-08 11:08:29 +0000

    change og image on join

--- frontend/src/app/api/join/og/route.tsx
+++ frontend/src/app/api/join/og/route.tsx
@@ -23,6 +23,10 @@ export async function GET(request: NextRequest) {
       new URL("../../../../../public/fonts/Inter-Bold.ttf", import.meta.url)
     ).then((res) => res.arrayBuffer());
 
+    const bgImage = await fetch(
+      new URL("../../../../../public/images/og-image-join.png", import.meta.url)
+    ).then((res) => res.arrayBuffer());
+
     return new ImageResponse(
       (
         <div
@@ -31,10 +35,15 @@ export async function GET(request: NextRequest) {
             width: "100%",
             display: "flex",
             flexDirection: "column",
-            alignItems: "center",
+            alignItems: "flex-start",
             justifyContent: "center",
-            background: "linear-gradient(180deg, #FFFFFF 0%, #CACACA 100%)",
+            backgroundImage: `url(data:image/png;base64,${Buffer.from(
+              bgImage
+            ).toString("base64")})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
             fontFamily: "Inter",
+            paddingLeft: "64px",
           }}
         >
           <div
@@ -42,152 +51,163 @@ export async function GET(request: NextRequest) {
               display: "flex",
               flexDirection: "row",
               alignItems: "center",
-              gap: "32px",
+              gap: "48px",
             }}
           >
-            {/* Profile Picture */}
             <div
               style={{
-                width: "200px",
-                height: "200px",
-                borderRadius: "100px",
-                border: "8px solid white",
-                marginBottom: "32px",
-                overflow: "hidden",
                 display: "flex",
+                flexDirection: "row",
                 alignItems: "center",
-                justifyContent: "center",
-                backgroundColor: "#f3f4f6",
+                gap: "32px",
+                marginBottom: "32px",
               }}
             >
-              {picture ? (
-                // eslint-disable-next-line @next/next/no-img-element
-                <img
-                  src={picture}
-                  alt={name || "Profile"}
-                  width={150}
-                  height={150}
+              {/* Profile Picture */}
+              <div
+                style={{
+                  width: "160px",
+                  height: "160px",
+                  borderRadius: "80px",
+                  border: "8px solid white",
+                  overflow: "hidden",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: "#f3f4f6",
+                }}
+              >
+                {picture ? (
+                  // eslint-disable-next-line @next/next/no-img-element
+                  <img
+                    src={picture}
+                    alt={name || "Profile"}
+                    width={150}
+                    height={150}
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "cover",
+                    }}
+                  />
+                ) : (
+                  <div
+                    style={{
+                      fontSize: "80px",
+                      color: "#9ca3af",
+                    }}
+                  >
+                    {name?.[0]?.toUpperCase() || "?"}
+                  </div>
+                )}
+              </div>
+
+              {/* Name and Username */}
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                }}
+              >
+                <span
                   style={{
-                    width: "100%",
-                    height: "100%",
-                    objectFit: "cover",
-                  }}
-                />
-              ) : (
-                <div
-                  style={{
-                    fontSize: "80px",
-                    color: "#9ca3af",
+                    fontSize: "48px",
+                    fontWeight: 700,
+                    color: "#111827",
+                    marginBottom: "8px",
                   }}
                 >
-                  {name?.[0]?.toUpperCase() || "?"}
-                </div>
-              )}
+                  {name}
+                </span>
+                <span
+                  style={{
+                    fontSize: "24px",
+                    color: "#6b7280",
+                  }}
+                >
+                  @{username}
+                </span>
+              </div>
             </div>
 
-            {/* Name and Username */}
+            {/* Stats */}
             <div
               style={{
                 display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
+                gap: "64px",
                 marginBottom: "48px",
+                marginTop: "16px",
+                alignItems: "flex-start",
+                marginRight: "48px",
               }}
             >
-              <span
+              <div
                 style={{
-                  fontSize: "64px",
-                  fontWeight: 700,
-                  color: "#111827",
-                  marginBottom: "8px",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
                 }}
               >
-                {name}
-              </span>
-              <span
+                <span
+                  style={{
+                    fontSize: "64px",
+                    fontWeight: 700,
+                    color: "#111827",
+                  }}
+                >
+                  {planCount}
+                </span>
+                <span
+                  style={{
+                    fontSize: "28px",
+                    color: "#6b7280",
+                  }}
+                >
+                  Plans
+                </span>
+              </div>
+              <div
                 style={{
-                  fontSize: "32px",
-                  color: "#6b7280",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
                 }}
               >
-                @{username}
-              </span>
+                <span
+                  style={{
+                    fontSize: "64px",
+                    fontWeight: 700,
+                    color: "#111827",
+                  }}
+                >
+                  {friendCount}
+                </span>
+                <span
+                  style={{
+                    fontSize: "28px",
+                    color: "#6b7280",
+                  }}
+                >
+                  Friends
+                </span>
+              </div>
             </div>
           </div>
-
-          {/* Stats */}
-          <div
-            style={{
-              display: "flex",
-              gap: "48px",
-              marginBottom: "32px",
-            }}
-          >
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <span
-                style={{
-                  fontSize: "48px",
-                  fontWeight: 700,
-                  color: "#111827",
-                }}
-              >
-                {planCount}
-              </span>
-              <span
-                style={{
-                  fontSize: "24px",
-                  color: "#6b7280",
-                }}
-              >
-                Plans
-              </span>
-            </div>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <span
-                style={{
-                  fontSize: "48px",
-                  fontWeight: 700,
-                  color: "#111827",
-                }}
-              >
-                {friendCount}
-              </span>
-              <span
-                style={{
-                  fontSize: "24px",
-                  color: "#6b7280",
-                }}
-              >
-                Friends
-              </span>
-            </div>
-          </div>
-
           {/* Plans Preview */}
           {currentlyWorkingOnEmoji && currentlyWorkingOnGoal && (
             <div
               style={{
                 display: "flex",
                 flexDirection: "column",
-                alignItems: "center",
+                alignItems: "flex-start",
                 gap: "16px",
+                marginRight: "48px",
               }}
             >
               <span
                 style={{
-                  fontSize: "24px",
+                  fontSize: "28px",
                   color: "#6b7280",
                 }}
               >
@@ -196,19 +216,21 @@ export async function GET(request: NextRequest) {
               <div
                 style={{
                   display: "flex",
-                  gap: "16px",
+                  gap: "24px",
                   background: "white",
-                  padding: "12px 24px",
-                  borderRadius: "16px",
+                  padding: "16px 32px",
+                  borderRadius: "20px",
                   alignItems: "center",
                 }}
               >
-                <span style={{ fontSize: "32px" }}>{currentlyWorkingOnEmoji}</span>
+                <span style={{ fontSize: "60px" }}>
+                  {currentlyWorkingOnEmoji}
+                </span>
                 <span
                   style={{
-                    fontSize: "24px",
+                    fontSize: "44px",
                     color: "#111827",
-                    maxWidth: "200px",
+                    maxWidth: "400px",
                     overflow: "hidden",
                     textOverflow: "ellipsis",
                     whiteSpace: "nowrap",
