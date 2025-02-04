a505f46ddde0938a2784ded04c2a673d8b9de6d0
alramalho <alexandre.ramalho.1998@gmail.com>
2025-02-04 08:57:48 +0000

    Adjust bottom navigation and add page bottom padding

--- frontend/src/app/add/page.tsx
+++ frontend/src/app/add/page.tsx
@@ -160,7 +160,7 @@ const LogPage: React.FC = () => {
         )}
       </div>
       <div className="mt-8">
-        <div className="fixed bottom-0 left-0 right-0 p-3 pb-[6.4rem] bg-white border-t-2 z-[45]">
+        <div className="fixed bottom-0 left-0 right-0 p-3 pb-[6.7rem] bg-white border-t-2 z-[45]">
           <Button
             onClick={handleLogActivity}
             className="w-full bg-black text-white h-10"
