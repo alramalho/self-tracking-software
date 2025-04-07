59d73a6139442b2d94fc0a0ff1e832c61dbcae13
alramalho <alexandre.ramalho.1998@gmail.com>
2025-04-07 14:21:36 +0100

    add missing dynamo config and fargate

--- aws-infrastructure/src/main-stack.ts
+++ aws-infrastructure/src/main-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { ApiStack } from "./api-stack";
+import { DbStack } from "./db-stack";
 
 interface MainStackProps {
   environment: string;
@@ -10,8 +11,15 @@ export class MainStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: MainStackProps) {
     super(scope, id);
 
-    new ApiStack(this, "ApiStack", {
+    // Deploy Lambda backend
+    const { fargateService, fastApiLambda } = new ApiStack(this, "ApiStack", {
       environment: props.environment,
+    });
+
+    new DbStack(this, "DbStack", {
+      environment: props.environment,
+      writableBy: [fastApiLambda, fargateService.taskDefinition.taskRole],
+      readableBy: [fastApiLambda, fargateService.taskDefinition.taskRole],
     });
   }
 }
