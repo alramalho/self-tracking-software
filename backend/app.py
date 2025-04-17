76a50057e911d22d414af2f0501b6652d6d33469
alramalho <alexandre.ramalho.1998@gmail.com>
2025-04-17 12:05:52 +0100

    update app.py

--- backend/app.py
+++ backend/app.py
@@ -184,7 +184,7 @@ def read_root():
     logger.info("Health check")
     return {"status": "ok"}
 
-@app.get("/exception")
+@app.get("/exception2")
 def read_root():
     raise HTTPException(status_code=500, detail="test")
     return {"status": "ok"}
