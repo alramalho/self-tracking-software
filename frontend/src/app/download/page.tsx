224b507603d808eccd069557aa705b1be11b7864
alramalho <alexandre.ramalho.1998@gmail.com>
2025-06-04 11:35:43 +0200

    desktop optimization

--- frontend/src/app/download/page.tsx
+++ frontend/src/app/download/page.tsx
@@ -4,15 +4,15 @@ import { Smartphone, X } from "lucide-react";
 
 const DownloadPage: React.FC = () => {
   return (
-  <div className="h-screen w-screen absolute flex z-[200] flex-col items-center justify-center px-4 bg-white overflow-hidden pointer-events-auto">
+    <div className="h-full w-full absolute flex z-[200] flex-col items-center justify-center px-4 bg-white overflow-hidden pointer-events-auto z-[1000]">
       <Smartphone className="w-16 h-16 mb-6 text-gray-600" />
 
       <div className="w-full max-w-md">
         <h2 className="text-2xl font-semibold mb-4 text-center">
-             To Download the app in
-            </h2>
-            <InstallPWATabs />
-          </div>
+          To Download the app in
+        </h2>
+        <InstallPWATabs />
+      </div>
     </div>
   );
 };
