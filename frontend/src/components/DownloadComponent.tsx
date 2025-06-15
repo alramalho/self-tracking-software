7efb4df97d0789e942aae4763dac4aef2b1157f0
alramalho <alexandre.ramalho.1998@gmail.com>
2025-06-15 13:03:24 +0200

    update DownloadComponent.tsx

--- frontend/src/components/DownloadComponent.tsx
+++ frontend/src/components/DownloadComponent.tsx
@@ -33,7 +33,7 @@ const DownloadComponent = () => {
     }
   };
 
-  if (!isDesktop)
+  if (isDesktop)
     return (
       <div className="flex flex-col gap-2 items-center">
         <div className="flex flex-row gap-2">
