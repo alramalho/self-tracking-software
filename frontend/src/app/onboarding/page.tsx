41d87c527c66f2b8f10711ed83c2c7912a83b375
alramalho <alexandre.ramalho.1998@gmail.com>
2025-06-21 09:31:43 +0200

    pricing changes

--- frontend/src/app/onboarding/page.tsx
+++ frontend/src/app/onboarding/page.tsx
@@ -94,12 +94,12 @@ const OnboardingStepRenderer = () => {
       />
       <ChevronLeft
         onClick={prevStep}
-        className="absolute top-2 left-2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 cursor-pointer"
+        className="fixed top-2 left-2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 cursor-pointer"
       />
       {isStepCompletedCallback && (
         <ChevronRight
           onClick={nextStep}
-          className="absolute top-2 right-2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 cursor-pointer"
+          className="fixed top-2 right-2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 cursor-pointer"
         />
       )}
       
