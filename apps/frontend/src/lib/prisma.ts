501d0cf00c285960c2dabc20b6703c08596348fd
alramalho <alexandre.ramalho.1998@gmail.com>
2025-08-22 18:52:46 +0200

    update prisma.ts

--- apps/frontend/src/lib/prisma.ts
+++ apps/frontend/src/lib/prisma.ts
@@ -3,6 +3,8 @@ import { PrismaClient } from "@tsw/prisma";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
