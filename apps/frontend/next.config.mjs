befc3b4c86aaee446da31fe79ee0d1056d2da81c
alramalho <alexandre.ramalho.1998@gmail.com>
2025-08-24 16:47:53 +0200

    update next.config.mjs

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -1,12 +1,12 @@
 // import withSerwistInit from "@serwist/next";
 import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
-// const withSerwist = withSerwistInit({
-//   swSrc: "src/app/sw.ts",
-//   swDest: "public/sw.js",
-//   register: false, // we'll do this manually
-//   cacheOnFrontEndNav: true,
-// });
+const withSerwist = withSerwistInit({
+  swSrc: "src/app/sw.ts",
+  swDest: "public/sw.js",
+  register: false, // we'll do this manually
+  cacheOnFrontEndNav: true,
+});
 
 const nextConfig = {
   async rewrites() {
@@ -39,5 +39,5 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
-// export default withSerwist(nextConfig);
+// export default nextConfig;
+export default withSerwist(nextConfig);
