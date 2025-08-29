07d0dd57696711a69e589ce3cb1789c55ca3e02e
alramalho <alexandre.ramalho.1998@gmail.com>
2025-08-29 10:02:46 +0200

    feat(prisma): add updatedAt field to Activity, ActivityEntry, and Plan models

---
+++ packages/prisma/migrations/20250829080155_add_updated_at/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `activities` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `activity_entries` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `plans` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."_ActivityToPlan" ADD CONSTRAINT "_ActivityToPlan_AB_pkey" PRIMARY KEY ("A", "B");
+
+-- DropIndex
+DROP INDEX "public"."_ActivityToPlan_AB_unique";
+
+-- AlterTable
+ALTER TABLE "public"."_PlanGroupToUser" ADD CONSTRAINT "_PlanGroupToUser_AB_pkey" PRIMARY KEY ("A", "B");
+
+-- DropIndex
+DROP INDEX "public"."_PlanGroupToUser_AB_unique";
+
+-- AlterTable
+ALTER TABLE "public"."activities" ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "public"."activity_entries" ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "public"."plans" ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
