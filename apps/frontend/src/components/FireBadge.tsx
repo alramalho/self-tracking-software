9464d32eaef711c5dd31bf563ce8a8d50c09b183
alramalho <alexandre.ramalho.1998@gmail.com>
2025-09-10 13:42:04 +0200

    refactor home plan badges

--- apps/frontend/src/components/FireBadge.tsx
+++ apps/frontend/src/components/FireBadge.tsx
@@ -1,6 +1,6 @@
+import { Badge } from "lucide-react";
 import React from "react";
 import Lottie from "react-lottie";
-import { Badge } from "./ui/badge";
 import fireAnimation from "../../public/animations/fire.lottie.json";
 
 interface FireBadgeProps {
@@ -8,26 +8,41 @@ interface FireBadgeProps {
   [key: string]: any;
 }
 
-const FireBadge: React.FC<FireBadgeProps> = ({ children, ...props }) => {
+export const FireAnimation = (props: {
+  height: number;
+  width: number;
+  className?: string;
+}) => {
   return (
-    <div className={`relative ${!children && "opacity-40 grayscale"}`} {...props}>
+    <div className={props.className}>
       <Lottie
         options={{
           loop: true,
           autoplay: true,
           animationData: fireAnimation,
           rendererSettings: {
-            preserveAspectRatio: "xMidYMid slice"
-          }
+            preserveAspectRatio: "xMidYMid slice",
+          },
         }}
-        height={60}
-        width={60}
+        height={props.height || 60}
+        width={props.width || 60}
       />
+    </div>
+  );
+};
+
+const FireBadge: React.FC<FireBadgeProps> = ({ children, ...props }) => {
+  return (
+    <div
+      className={`relative ${!children && "opacity-40 grayscale"}`}
+      {...props}
+    >
+      <FireAnimation height={props.height || 60} width={props.width || 60} />
       {children && (
         <Badge className="absolute bottom-0 left-1/2 -translate-x-1/2 text-sm bg-black/60">
           {children}
         </Badge>
-      )}
+      )}{" "}
     </div>
   );
 };
