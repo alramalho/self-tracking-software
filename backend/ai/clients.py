adc69936e6a0282bd38c089674d12e82dc632d68
alramalho <alexandre.ramalho.1998@gmail.com>
2025-09-16 10:01:45 +0200

    rm backend

--- backend/ai/clients.py
+++
@@ -1,8 +0,0 @@
-
-
-from openai import AsyncOpenAI, OpenAI
-from constants import OPENAI_API_KEY  
-
-
-async_openai_client = AsyncOpenAI(api_key=OPENAI_API_KEY)
-sync_openai_client = OpenAI(api_key=OPENAI_API_KEY)
--- backend/scripts/update-clerk-id.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This script is now a wrapper for the aws-tracking-software Python command
-# It maintains backward compatibility while using the new Python module
-
-aws-update-item -t tracking_software_users_dev --primary_key_name "id" --primary_key_value "670fb420158ba86def604e67" --key_name "clerk_id" --old_value "user_2nWIRvGq87GHdd8LlSbt7cCOvRf" --new_value "user_2kUW1zytLj9ERvDqVDDFCvIp5Un"
-exit $? 
