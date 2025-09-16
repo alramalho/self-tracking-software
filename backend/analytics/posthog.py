bbd19e0434a6d269771eb50f769d9b2a5ed00672
alramalho <alexandre.ramalho.1998@gmail.com>
2025-09-16 10:01:45 +0200

    rm backend

--- backend/analytics/posthog.py
+++
@@ -1,7 +0,0 @@
-from posthog import Posthog
-from constants import POSTHOG_API_KEY, POSTHOG_HOST
-posthog = Posthog(
-    project_api_key=POSTHOG_API_KEY,
-    host=POSTHOG_HOST,
-)
-
--- backend/scripts/run_tests.sh
+++
@@ -1,2 +0,0 @@
-python -m pytest -vv -s -x tests/test_flowchart_framework.py
-python -m pytest -vv -s -x --ignore=tests/test_week_analyser.py --ignore=tests/test_flowchart_framework.py
