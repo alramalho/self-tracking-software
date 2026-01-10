74c2fc007336f479748d371f317ad6a6ddddcef8
alex <alexandre.ramalho.1998@gmail.com>
2026-01-10 12:35:39 +0000

    feat: add pause feature columns to plans table in migration

--- packages/prisma/migrations/20260110000000_add_plan_pause_feature/migration.sql
+++ packages/prisma/migrations/20260110000000_add_plan_pause_feature/migration.sql
@@ -2,4 +2,3 @@
 ALTER TABLE "public"."plans" ADD COLUMN IF NOT EXISTS "isPaused" BOOLEAN NOT NULL DEFAULT false;
 ALTER TABLE "public"."plans" ADD COLUMN IF NOT EXISTS "pauseReason" TEXT;
 ALTER TABLE "public"."plans" ADD COLUMN IF NOT EXISTS "pauseHistory" JSONB;
-11
